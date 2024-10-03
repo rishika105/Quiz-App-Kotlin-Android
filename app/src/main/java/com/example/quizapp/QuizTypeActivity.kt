1a380242d1174ac9cc53831e5aff18c33876546d
arpita-32 <[arpitasundaray9@gmail.com]>
2024-10-03 13:31:08 +0530

    added quiz type kt file by rishika

--- app/src/main/java/com/example/quizapp/QuizTypeActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizTypeActivity.kt
@@ -1,20 +1,54 @@
 package com.example.quizapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class QuizTypeActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_quiz_type)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val userName = intent.getStringExtra(Constants.USER_NAME)
+
+        val btnFlags = findViewById<Button>(R.id.btnFlags)
+        val btnGeneralKnowledge = findViewById<Button>(R.id.btnGeneralKnowledge)
+        val btnVerbal = findViewById<Button>(R.id.btnVerbal)
+        val btnReasoning = findViewById<Button>(R.id.btnReasoning)
+
+        btnFlags.setOnClickListener {
+            val intent = Intent(this, QuizQuestionsActivity::class.java)
+            intent.putExtra(Constants.USER_NAME, userName)
+            intent.putExtra(Constants.QUIZ_TYPE, Constants.QuizType.FLAGS.toString())
+            startActivity(intent)
+            finish()
+        }
+
+        btnGeneralKnowledge.setOnClickListener {
+            val intent = Intent(this, QuizQuestionsActivity::class.java)
+            intent.putExtra(Constants.USER_NAME, userName)
+            intent.putExtra(Constants.QUIZ_TYPE, Constants.QuizType.GENERAL_KNOWLEDGE.toString())
+            startActivity(intent)
+            finish()
+        }
+
+        btnVerbal.setOnClickListener {
+            val intent = Intent(this, QuizQuestionsActivity::class.java)
+            intent.putExtra(Constants.USER_NAME, userName)
+            intent.putExtra(Constants.QUIZ_TYPE, Constants.QuizType.VERBAL.toString())
+            startActivity(intent)
+            finish()
+        }
+
+        btnReasoning.setOnClickListener {
+            val intent = Intent(this, QuizQuestionsActivity::class.java)
+            intent.putExtra(Constants.USER_NAME, userName)
+            intent.putExtra(Constants.QUIZ_TYPE, Constants.QuizType.REASONING.toString())
+            startActivity(intent)
+            finish()
         }
     }
 }
