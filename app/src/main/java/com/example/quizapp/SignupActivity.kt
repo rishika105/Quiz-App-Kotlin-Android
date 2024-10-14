53bc52971b7b79d98cfe1e112b7cf7224d91e186
arpita-32 <arpitasundaray9@gmail.com>
2024-10-14 12:28:19 +0530

    added signup file

---
+++ app/src/main/java/com/example/quizapp/SignupActivity.kt
@@ -0,0 +1,62 @@
+package com.example.quizapp;
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.quizapp.databinding.ActivitySignupBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        binding.signupButton.setOnClickListener {
+            val email = binding.signupEmail.text.toString()
+            val password = binding.signupPassword.text.toString()
+            val confirmPassword = binding.signupConfirm.text.toString()
+
+            if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(this, "All fields are mandatory", Toast.LENGTH_SHORT).show()
+            } else {
+                if (password == confirmPassword) {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val checkUserEmail = databaseHelper.checkEmail(email)
+                        if (!checkUserEmail) {
+                            val insert = databaseHelper.insertData(email, password)
+                            withContext(Dispatchers.Main) {
+                                if (insert) {
+                                    Toast.makeText(this@SignupActivity, "Signup Successfully!", Toast.LENGTH_SHORT).show()
+                                    startActivity(Intent(this@SignupActivity, LoginActivity::class.java))
+                                    finish()
+                                } else {
+                                    Toast.makeText(this@SignupActivity, "Signup Failed!", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        } else {
+                            withContext(Dispatchers.Main) {
+                                Toast.makeText(this@SignupActivity, "User  already exists! Please login", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.loginRedirectText.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
